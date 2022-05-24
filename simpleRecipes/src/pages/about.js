8250da9acc88bf875bb8365d379600dbabfad6ba
Jason Evans <evans.jason@hotmail.co.uk>
2022-05-24 10:29:58 +0100

    layout: wrapper used to display navbar and footer on all pages

--- simpleRecipes/src/pages/about.js
+++ simpleRecipes/src/pages/about.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import Layout from "../components/Layout"
 
 export default function about() {
   return (
-    <div>
-      <Navbar />
+    <Layout>
       <h1>About Page</h1>
-    </div>
+    </Layout>
   )
 }
--- simpleRecipes/src/pages/contact.js
+++ simpleRecipes/src/pages/contact.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import Layout from "../components/Layout"
 
 export default function contact() {
   return (
-    <div>
-      <Navbar />
+    <Layout>
       <h1>Contact Page</h1>
-    </div>
+    </Layout>
   )
 }
