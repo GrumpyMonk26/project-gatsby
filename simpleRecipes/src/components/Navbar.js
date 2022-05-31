9467157cfe9a81de56805044e70edc5a6867afd0
Jason Evans <evans.jason@hotmail.co.uk>
2022-05-31 11:18:22 +0100

    setup: navbar reconfigured, added logo image, used react icons for the burger icon

--- simpleRecipes/src/components/Navbar.js
+++ simpleRecipes/src/components/Navbar.js
@@ -1,26 +1,60 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FiAlignJustify } from "react-icons/fi"
+import logo from "../assets/images/logo.svg"
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/recipes">Recipes</Link>
-        </li>
-        <li>
-          <Link to="/tags">Tags</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simple recipes" />
+          </Link>
+          <button className="nav-btn">
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            Home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+          >
+            Recipes
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            Tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            About
+          </Link>
+        </div>
+      </div>
     </nav>
+    // <nav>
+    //   <FiAlignJustify />
+    //   <img src="" alt="Logo" />
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/about">About</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/contact">Contact</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/recipes">Recipes</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/tags">Tags</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
   )
 }
