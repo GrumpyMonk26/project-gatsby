56c54efee6ff9afbc92183b3fc182b86f15c301e
Jason Evans <evans.jason@hotmail.co.uk>
2022-05-31 10:41:38 +0100

    setup: contact page setup with a simle form and submit button

--- simpleRecipes/src/pages/contact.js
+++ simpleRecipes/src/pages/contact.js
@@ -4,7 +4,44 @@ import Layout from "../components/Layout"
 export default function contact() {
   return (
     <Layout>
-      <h1>Contact Page</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want To Get In Touch?</h3>
+            <p>
+              If you have any questions, comments, or concerns, please feel free
+              to
+            </p>
+            <p>
+              voluptatem alias illo temporibus ut obcaecati pariatur similique
+            </p>
+            <p>
+              harum architecto tempora porro cumque aspernatur aperiam quae
+              deserunt repellendus natus enim esse, dicta dolorum sint! Impedit
+              a
+            </p>
+          </article>
+          <article>
+            <form action="" className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">Your Name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">Your Email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">Your Message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                Submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
