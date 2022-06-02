9c9aaf3e7b1f17024f9d5f2fbc3d44166f5aa3b5
Jason Evans <evans.jason@hotmail.co.uk>
2022-06-02 10:11:54 +0100

    design: about page with 2 columns, image handled using StaticImage using the gatsby plugin

--- simpleRecipes/src/pages/about.js
+++ simpleRecipes/src/pages/about.js
@@ -1,10 +1,36 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
-export default function about() {
+export default function About() {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium quis omnis nesciunt perferendis nam fugit veniam
+              incidunt totam deserunt excepturi.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur blanditiis architecto alias!
+            </p>
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="person pouring salt from a bowl"
+            className="about-img"
+            placeholder="tracedSVG"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
